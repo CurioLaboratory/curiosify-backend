f309a78bdd8b96e7b301226ef8a836a89c6472c4
Ramanathan A <80613460+ramanathan-007@users.noreply.github.com>
2024-10-10 20:11:45 +0530

    Update server.js

--- server.js
+++ server.js
@@ -27,5 +27,5 @@ redisClient.on('error', (err) => {
 
 const port = process.env.PORT || 5001;
 app.listen(port, () => {
-    console.log(Server running at http://localhost:${port}/);
+    console.log(`Server running at http://localhost:${port}/`);
 });
