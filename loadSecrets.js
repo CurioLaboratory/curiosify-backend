e278dad8ba09b3c18d0e51127925345e2d8dbac5
Abhishek Pal <131875068+Abhipal7500@users.noreply.github.com>
2024-10-22 15:01:01 +0530

    Update loadSecrets.js

--- loadSecrets.js
+++ loadSecrets.js
@@ -1,4 +1,3 @@
-// loadSecrets.js
 const AWS = require('aws-sdk');
 
 // Set the AWS region
@@ -8,15 +7,20 @@ const secretsManager = new AWS.SecretsManager();
 
 async function loadSecrets() {
   try {
-    const data = await secretsManager.getSecretValue({ SecretId: 'curiosify-backend-secrets' }).promise();
+    const data = await secretsManager.getSecretValue({ SecretId: 'curiosify-backend-secrets-lrW2ce' }).promise();
+    
+    // Parse the secrets from SecretString
     const secrets = JSON.parse(data.SecretString);
     
     // Assign the secrets to process.env
     Object.keys(secrets).forEach((key) => {
       process.env[key] = secrets[key];
     });
+
+    console.log("Secrets successfully loaded and assigned to environment variables.");
   } catch (err) {
     console.error("Error retrieving secrets:", err);
+    throw err; // Re-throw the error to be handled in server.js
   }
 }
 
