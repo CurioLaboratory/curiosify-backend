bbc6ca386f6d2e3cf43a87c2c3b13b1697d34ac2
Abhishek Pal <131875068+Abhipal7500@users.noreply.github.com>
2024-10-22 16:16:49 +0530

    Update index.js

--- controllers/auth/index.js
+++ controllers/auth/index.js
@@ -1,11 +1,45 @@
-require("dotenv").config();
+
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 const nodemailer = require("nodemailer");
 const User = require("../../models/auth/User");
 const redisClient =require("../../redisclient")
+const AWS = require('aws-sdk');
+const { SecretsManager } = require('aws-sdk');
+
+// Set your region
+const region = 'us-east-1'; // Change this to your desired region
+AWS.config.update({ region });
+
+const secretsManager = new SecretsManager();
+
+const loadSecrets = async () => {
+    try {
+        const data = await secretsManager.getSecretValue({ SecretId: 'curiosify-backend-secrets' }).promise();
+        if ('SecretString' in data) {
+            const secret = JSON.parse(data.SecretString);
+            // Set environment variables
+            process.env.MONGO_STR = secret.MONGO_STR; 
+            process.env.JWT_SECRET = secret.JWT_SECRET;
+            process.env.JWT_EXPIRES_IN = secret.JWT_EXPIRES_IN;
+            process.env.FRONTEND_URL = secret.FRONTEND_URL;
+            process.env.REDIS_URL = secret.REDIS_URL;
+            process.env.SES_HOST = secret.SES_HOST;
+            process.env.SES_PORT = secret.SES_PORT;
+            process.env.SES_USER = secret.SES_USER;
+            process.env.SES_PASS = secret.SES_PASS;
+        
+        }
+    } catch (err) {
+        console.error('Error retrieving secrets:', err);
+    }
+};
+
+
+
 exports.signup = async (req, res) => {
     try {
+         await loadSecrets();
         const { name, email, password, role, collegeName } = req.body;
 
         // Restricted domains (update this list with the domains you want to restrict)
