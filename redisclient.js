bfb7b258ebe11effe84bc1b8623c04fa097d1bcc
Abhishek Pal <131875068+Abhipal7500@users.noreply.github.com>
2024-10-22 16:56:13 +0530

    Update redisclient.js

--- redisclient.js
+++ redisclient.js
@@ -1,6 +1,38 @@
 // redis.js
 const redis = require('redis');
-const redisURl=process.env.REDISS_URL;
+const AWS = require('aws-sdk');
+const { SecretsManager } = require('aws-sdk');
+
+// Set your region
+const region = 'us-east-1'; // Change this to your desired region
+AWS.config.update({ region });
+
+const secretsManager = new SecretsManager();
+
+const loadSecrets = async () => {
+    try {
+        const data = await secretsManager.getSecretValue({ SecretId: 'curiosify-backend-secrets' }).promise();
+        if ('SecretString' in data) {
+            const secret = JSON.parse(data.SecretString);
+
+            // Set environment variables
+            process.env.MONGO_STR = secret.MONGO_STR; 
+            process.env.JWT_SECRET = secret.JWT_SECRET;
+            process.env.JWT_EXPIRES_IN = secret.JWT_EXPIRES_IN;
+            process.env.FRONTEND_URL = secret.FRONTEND_URL;
+            process.env.REDIS_URL = secret.REDIS_URL;
+            process.env.SES_HOST = secret.SES_HOST;
+            process.env.SES_PORT = secret.SES_PORT;
+            process.env.SES_USER = secret.SES_USER;
+            process.env.SES_PASS = secret.SES_PASS;
+        }
+    } catch (err) {
+        console.error('Error retrieving secrets:', err);
+    }
+};
+
+  await loadSecrets();
+const redisURl=process.env.REDIS_URL;
 const redisClient = redis.createClient({
 
   url: redisURl, 
@@ -13,6 +45,7 @@ redisClient.on('error', (err) => {
 
 const connectRedis = async () => {
   try {
+    await loadSecrets();
     await redisClient.connect();
     console.log('Connected to Redis');
     console.log(redisURl);
