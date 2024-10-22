e701a0c9977abc9e76617ce9fcdea1c0a8d02866
Abhishek Pal <131875068+Abhipal7500@users.noreply.github.com>
2024-10-22 16:20:51 +0530

    Update redisclient.js

--- redisclient.js
+++ redisclient.js
@@ -1,47 +1,26 @@
+// redis.js
 const redis = require('redis');
-const AWS = require('aws-sdk');
-const { SecretsManager } = require('aws-sdk');
+const redisURl=process.env.REDISS_URL;
+const redisClient = redis.createClient({
 
-// Set your region
-const region = 'us-east-1'; 
-AWS.config.update({ region });
+  url: redisURl, 
+});
 
-const secretsManager = new SecretsManager();
 
-const loadSecrets = async () => {
-    try {
-        const data = await secretsManager.getSecretValue({ SecretId: 'curiosify-backend-secrets' }).promise();
-        if ('SecretString' in data) {
-            const secret = JSON.parse(data.SecretString);
-
-            // Set environment variables
-            process.env.REDIS_URL = secret.REDIS_URL;
-        }
-    } catch (err) {
-        console.error('Error retrieving secrets:', err);
-    }
-};
+redisClient.on('error', (err) => {
+  console.error('Redis error:', err);
+});
 
 const connectRedis = async () => {
-    await loadSecrets(); // Load secrets before connecting to Redis
-
-    const redisURL = process.env.REDIS_URL; 
-    const redisClient = redis.createClient({
-        url: redisURL,
-    });
-
-    redisClient.on('error', (err) => {
-        console.error('Redis error:', err);
-    });
-
-    try {
-        await redisClient.connect();
-        console.log('Connected to Redis');
-        return redisClient; // Return the Redis client instance after connection
-    } catch (err) {
-        console.error('Error connecting to Redis:', err);
-        throw err; // Throw the error to be caught by calling function
-    }
+  try {
+    await redisClient.connect();
+    console.log('Connected to Redis');
+    console.log(redisURl);
+  } catch (err) {
+    console.error('Error connecting to Redis:', err);
+  }
 };
 
-module.exports = connectRedis;
+connectRedis();
+
+module.exports = redisClient;
