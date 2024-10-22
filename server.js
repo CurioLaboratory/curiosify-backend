8c489e5deb4236d9c82c985f7aeb2bc7b1bcba8c
Abhishek Pal <131875068+Abhipal7500@users.noreply.github.com>
2024-10-22 15:55:21 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-require("dotenv").config();
+
 const express = require("express");
 const cors = require("cors");
 const loadSecrets = require('./loadSecrets.js');  // Your AWS Secrets Manager loader module
