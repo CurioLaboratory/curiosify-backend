a0dfda5b1ea4f91954b13095e171881e88f92df4
Amit_Bisht <singhbishtamit2@gmail.com>
2024-11-28 16:51:19 +0530

    feat: added quiz genration using openAI

--- routes/quiz/index.js
+++ routes/quiz/index.js
@@ -1,11 +1,19 @@
 const express = require("express");
 const router = express.Router();
-const middleware = require('../../middlewares/index');
-const quizControllers = require('../../controllers/quiz/index');
+const middleware = require("../../middlewares/index");
+const quizControllers = require("../../controllers/quiz/index");
 
-router.route('/createmanualquiz').post(middleware.fetchuser, quizControllers.createManualQuiz);
-router.route('/getallquiz').get(middleware.fetchuser, quizControllers.getAllQuiz);
-router.route('/deletequiz/:id').delete(middleware.fetchuser, quizControllers.deleteQuiz);
-router.route('/createAIquiz').post(middleware.fetchuser,quizControllers.createAIquiz);
-
+router
+  .route("/createmanualquiz")
+  .post(middleware.fetchuser, quizControllers.createManualQuiz);
+router
+  .route("/getallquiz")
+  .get(middleware.fetchuser, quizControllers.getAllQuiz);
+router
+  .route("/deletequiz/:id")
+  .delete(middleware.fetchuser, quizControllers.deleteQuiz);
+router
+  .route("/createAIquiz")
+  .post(middleware.fetchuser, quizControllers.createAIquiz);
+router.route("/genrateQuizUsingAI").post(quizControllers.genrateAIquize);
 module.exports = router;
