30a3d15dd2e05f033d0a777a063a55e496b65348
kushal67 <106881980+kushal67@users.noreply.github.com>
2024-12-05 10:31:02 +0530

    Update redisclient.js

--- redisclient.js
+++ redisclient.js
@@ -1,35 +1,35 @@
 // redis.js
 const redis = require('redis');
-const AWS = require('aws-sdk');
-const { SecretsManager } = require('aws-sdk');
+// const AWS = require('aws-sdk');
+// const { SecretsManager } = require('aws-sdk');
 
-// Set your region
-const region = 'us-east-1'; // Change this to your desired region
-AWS.config.update({ region });
+// // Set your region
+// const region = 'us-east-1'; // Change this to your desired region
+// AWS.config.update({ region });
 
-const secretsManager = new SecretsManager();
+// const secretsManager = new SecretsManager();
 
-const loadSecrets = async () => {
-    try {
-        const data = await secretsManager.getSecretValue({ SecretId: 'curiosify-backend-secrets' }).promise();
-        if ('SecretString' in data) {
-            const secret = JSON.parse(data.SecretString);
+// const loadSecrets = async () => {
+//     try {
+//         const data = await secretsManager.getSecretValue({ SecretId: 'curiosify-backend-secrets' }).promise();
+//         if ('SecretString' in data) {
+//             const secret = JSON.parse(data.SecretString);
 
-            // Set environment variables
-            process.env.MONGO_STR = secret.MONGO_STR; 
-            process.env.JWT_SECRET = secret.JWT_SECRET;
-            process.env.JWT_EXPIRES_IN = secret.JWT_EXPIRES_IN;
-            process.env.FRONTEND_URL = secret.FRONTEND_URL;
-            process.env.REDIS_URL = secret.REDIS_URL;
-            process.env.SES_HOST = secret.SES_HOST;
-            process.env.SES_PORT = secret.SES_PORT;
-            process.env.SES_USER = secret.SES_USER;
-            process.env.SES_PASS = secret.SES_PASS;
-        }
-    } catch (err) {
-        console.error('Error retrieving secrets:', err);
-    }
-};
+//             // Set environment variables
+//             process.env.MONGO_STR = secret.MONGO_STR; 
+//             process.env.JWT_SECRET = secret.JWT_SECRET;
+//             process.env.JWT_EXPIRES_IN = secret.JWT_EXPIRES_IN;
+//             process.env.FRONTEND_URL = secret.FRONTEND_URL;
+//             process.env.REDIS_URL = secret.REDIS_URL;
+//             process.env.SES_HOST = secret.SES_HOST;
+//             process.env.SES_PORT = secret.SES_PORT;
+//             process.env.SES_USER = secret.SES_USER;
+//             process.env.SES_PASS = secret.SES_PASS;
+//         }
+//     } catch (err) {
+//         console.error('Error retrieving secrets:', err);
+//     }
+// };
 (async () => {
     try {
         await loadSecrets(); // Connect to MongoDB after secrets are loaded
