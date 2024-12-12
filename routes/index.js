af5bf3f93c7289d509b91890373720b050c7db0f
Kushal <kushalagarwalab@gmail.com>
2024-12-12 20:28:43 +0530

    flashcard route

--- routes/index.js
+++ routes/index.js
@@ -18,7 +18,7 @@ router.use(
 router.use("/notification", require("./notification/index"));
 router.use("/library", require("./library/index"));
 router.use("/studentManagement", require("./studentManagement/index"));
-router.use("/flashcard", require("./flashcards/index"));
+router.use("/flashcard",upload.single("file"), require("./flashcards/index"));
 router.use("/useractivityFeed", require("./userActivityFeed/index"));
 router.use("/quizperformance", require("./quizperformance/index"));
 router.use("/userChatHistory", require("./userChatHistory/index"));
